3b480ebe7a57d40419633e9108a3249c3575b61d
Oleg Ershov <oecorango@gmail.com>
2023-11-22 01:07:45 +0400

    feat: add webpack loader for image compression

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,27 @@ module.exports = {
         generator: {
           filename: 'assets/image/[name][ext]',
         },
+        use: {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              progressive: true,
+            },
+            optipng: {
+              enabled: false,
+            },
+            pngquant: {
+              quality: [0.65, 0.90],
+              speed: 4,
+            },
+            gifsicle: {
+              interlaced: false,
+            },
+            webp: {
+              quality: 75,
+            },
+          },
+        },
       },
       {
         test: /\.(mp3|wav|ogg)$/i,
